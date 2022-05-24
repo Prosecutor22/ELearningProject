b9ef165f5f1c8fa53fd348b31090be323fa64a25
Nguyễn Đức An <86992472+anduc146khmt@users.noreply.github.com>
2022-05-24 22:56:40 +0700

    Update preprocessing.py

--- src/KafkaConsumertoMongoDB/preprocessing.py
+++ src/KafkaConsumertoMongoDB/preprocessing.py
@@ -1,7 +1,7 @@
 index = 1
 with open('logs.txt') as infile:
-   for line in infile:
-        line = line.replace('\n', '')
+   for line in infile:      
+        line = line.replace('\n', '').replace('\x00', '')
         if(line in ['[]', '', ' '] or (not line.startswith('['))):
             continue
         with open('log{}.json'.format(index), 'w') as outfile:
